f6e936cf90bb100d264c416e83e1d618ac5595d1
alvintang0925 <alvintang0925@gmail.com>
2020-11-24 14:37:53 +0800

    Initial Commit

---
+++ QTS TRENDLINE/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  QTS TRENDLINE
+//
+//  Created by 唐健恆 on 2020/11/24.
+//  Copyright © 2020 Alvin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
